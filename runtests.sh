b3131a3ca964df7f7264ac5d1d12e3ab34e258ee
Hassan Salehe Matar <hassansalehe@gmail.com>
2017-12-14 21:20:07 +0300

    Add unit tests

---
+++ runtests.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2017, Hassan Salehe Matar
+# All rights reserved.
+#
+# This file is part of FlowSanitizer. For details, see
+# https://github.com/hassansalehe/FlowSanitizer. Please also see the LICENSE file
+# for additional BSD notice
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+cd src/runtime
+./install.sh
+
+mkdir -p build
+cd -
+cd src/runtime/build
+rm -rf libLogger.a
+CXX=clang++ cmake ..
+make
+
+cd -
+cd src/detector
+mkdir -p build
+cd -
+cd src/detector/build
+CXX=clang++ cmake ..
+make
+
+cd -
+mkdir -p build
+cd build
+rm -rf libFlowSanitizer.so
+CXX=clang++ cmake ../src/tsan
+make
+
+cd ..
+reset
+
+TESTS=($(grep -l main src/tests/*.cc))
+for test in "${TESTS[@]}"; do
+  rm -rf Trace* HBlog*
+  rm -rf a.out
+  echo "RUNNING $test"
+  flowsan $test 
+  ./a.out
+  bin/DFchecker Trace* HBlog* IIR.iir
+done
+
---
+++ src/tests/parallel.cc
@@ -0,0 +1,67 @@
+//////////////////////////////////////////////////////////////
+//
+// parallel.cc
+//
+// Copyright (c) 2017, Hassan Salehe Matar
+// All rights reserved.
+//
+// This file is part of FlowSanitizer. For details, see
+// https://github.com/hassansalehe/FlowSanitizer. Please also
+// see the LICENSE file for additional BSD notice
+//
+// Redistribution and use in source and binary forms, with
+// or without modification, are permitted provided that
+// the following conditions are met:
+//
+// * Redistributions of source code must retain the above
+//   copyright notice, this list of conditions and the
+//   following disclaimer.
+//
+// * Redistributions in binary form must reproduce the
+//   above copyright notice, this list of conditions and
+//   the following disclaimer in the documentation and/or
+//   other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names
+//   of its contributors may be used to endorse or promote
+//   products derived from this software without specific
+//   prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+// CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////
+
+// This program includes the "parallel" construct.
+// In thia example each thread prints its own thread id.
+//
+// References:
+//  1. http://www.openmp.org/wp-content/uploads/openmp-examples-4.5.0.pdf
+//  2. http://www.openmp.org/wp-content/uploads/openmp-4.5.pdf
+
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+
+  int count = 0;
+  #pragma omp parallel shared(count)
+  {
+    count++;
+  }
+
+  printf("Value of count: %d, construct: <parallel>\n", count);
+  return 0;
+}
