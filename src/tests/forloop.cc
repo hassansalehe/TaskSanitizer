09b47821ae7ac292b27cf7ab705721a75faa747c
Hassan Salehe Matar <hassansalehe@gmail.com>
2018-01-09 13:23:16 +0300

    For loop in different method

--- src/tests/forloop.cc
+++ src/tests/forloop.cc
@@ -57,14 +57,19 @@
 #include <stdio.h>
 #include <omp.h>
 
-int main() {
+int count = 0;
 
-  int count = 0;
-  #pragma omp parallel num_threads(4) shared(count)
-  {
+void forMethod() {
     #pragma omp for
       for(int i = 0; i < 4; i++)
         count++;
+}
+
+int main() {
+
+  #pragma omp parallel num_threads(4) shared(count)
+  {
+    forMethod();
   }
 
   printf("Value of count: %d, construct: <forloop>\n", count);
