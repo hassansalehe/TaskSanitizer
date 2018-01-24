cdf8e1bea48843935f17284c82d6c4cb6cbfe389
Hassan Salehe Matar <hassansalehe@gmail.com>
2018-01-24 17:05:22 +0300

    Add benchmarks and evaluation script

---
+++ evaluation.py
@@ -0,0 +1,71 @@
+import os
+import subprocess
+import re
+import sys
+
+class Correctness( object ):
+    """
+    This class implements the functions for running instrumented version
+    of the benchmark applications for reporting of bugs from them.
+
+    It is launced if you provide "correctness" as the first argument
+    if the evaluation Python script.
+
+    It works in three modes:
+       (1) It runs all the benchmark programs if no specific benchmark
+           is specified. Example command: ./evaluation.py correctness
+       (2) It runs specified benchmark application.
+           Example: ./evaluation.py correctness fibonacci
+       (3) It runs a specified benchmark application with a speficied
+           input size. E.g; ./evaluation.py correctness fibonacci 400
+    """
+    def initialize( self ):
+        self.app_path = "./src/tests/benchmarks/"
+        self.apps = os.listdir( self.app_path )
+        self.inputSize = "25"
+        self.results = []
+
+    def parseArguments( self ):
+        if len(sys.argv) > 1:
+            self.apps = [sys.argv[1] + ".cc"];
+        if(len(sys.argv) > 2):
+            self.inputSizes = [sys.argv[2]]
+
+    def execute( self, commands):
+        p = subprocess.Popen(commands, stdout=subprocess.PIPE)
+        return p.communicate()
+
+    def findBugs( self, report):
+        regex = "lines:"
+        bugs = 0
+        for line in report.splitlines():
+            if regex in line:
+                bugs += 1
+        return bugs
+
+    def getNumTasks( self, report):
+        regex = "Total number of tasks:"
+        bugs = 0
+        for line in report.splitlines():
+            if regex in line:
+                return re.findall('\d+', line)[0]
+
+    def runExperiments( self ):
+        head = "Application, Input size, # tasks, # bugs"
+        print head
+        for app in self.apps:
+            fpath = self.app_path + app
+            output, err = self.execute( ["./flowsan", fpath] )
+
+            if err is None:
+                output, err = self.execute( ["./a.out", self.inputSize] )
+                num_bugs  = self.findBugs( output )
+                num_tasks = self.getNumTasks( output )
+                app_name = app.replace(".cc", "")
+                print app_name,",", self.inputSize,",", num_tasks,",", num_bugs
+
+if __name__ == "__main__":
+    correctness = Correctness()
+    correctness.initialize()
+    correctness.parseArguments()
+    correctness.runExperiments()
---
+++ src/tests/benchmarks/pointer_chasing.cc
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <iostream>
+
+typedef struct elem_t {
+  void * data;
+  elem_t * next;
+} elem_t;
+
+namespace pchase {
+
+void process(elem_t * elem) {
+  printf("I am a task %p\n " ,elem);
+}
+
+void process_list(elem_t *elem) {
+  #pragma omp parallel
+  {
+    #pragma omp single
+    {
+      while ( elem != NULL ) {
+        #pragma omp task
+        {
+          // elem is firstprivate by default
+          process( elem );
+        }
+        elem = elem->next;
+      }
+    }
+  }
+}
+
+elem_t * initialize(int n) {
+  elem_t *head = NULL;
+
+  for (int i = 0 ; i < n; i++) {
+    elem_t *elem = new elem_t;
+    elem->next = head;
+    head = elem;
+  }
+
+  return head;
+}
+
+void clean(elem_t *elem) {
+  while (elem) {
+    elem_t * tmp = elem->next;
+    delete elem;
+    elem = tmp;
+  }
+}
+
+}; // end namespace pchase
+
+int main(int argc, char **argv) {
+  int n = 14;
+  elem_t * elems_head = pchase::initialize(n);
+  pchase::process_list(elems_head);
+  pchase::clean(elems_head);
+  return 0;
+}
