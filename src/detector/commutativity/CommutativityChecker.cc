cebe81e5361ca22a31fbd3ccd7e9ffe67bfbca58
Hassan Matar <hassansalehe@gmail.com>
2018-03-17 15:38:47 +0000

    Fix commutative typo

--- src/detector/commutativity/CommutativityChecker.cc
+++ src/detector/commutativity/CommutativityChecker.cc
@@ -10,24 +10,27 @@
 //
 /////////////////////////////////////////////////////////////////
 
-// Defines Validator class which verifies that bugs detected are real.
+// Defines a class which verifies that bugs detected are not commutative
 
 // includes and definitions
 #include "detector/commutativity/CommutativityChecker.h"
 #include "detector/nondeterminism/conflict.h"
 #include "detector/nondeterminism/report.h"
 
+/**
+ * Parses IIR representation file for critical sections
+ */
 VOID CommutativityChecker::parseTasksIR(char * IRlogName) {
-  std::vector<Instruction> currentTask;
-  std::string sttmt; // program statement
-  std::ifstream IRcode(IRlogName); //  open IRlog file
+  std::vector<Instruction>  currentTask;
+  std::string               sttmt;             // program statement
+  std::ifstream             IRcode(IRlogName); // open IRlog file
 
   while ( getline(IRcode, sttmt) ) {
-    if ( isEmpty(sttmt) ) continue; // skip empty line
-    if ( isDebugCall(sttmt) ) continue; // skip debug call
+    if ( isEmpty(sttmt) ) continue;            // skip empty line
+    if ( isDebugCall(sttmt) ) continue;        // skip debug call
 
-    sttmt = Instruction::trim( sttmt ); // trim spaces
-    if ( isValidStatement(sttmt) ) { // check if normal statement
+    sttmt = Instruction::trim( sttmt );        // trim spaces
+    if ( isValidStatement(sttmt) ) {           // check if normal statement
       INTEGER lineNo = getLineNumber( sttmt );
       // skip instruction with line # 0: args to task body
       if (lineNo <= 0) continue;
@@ -40,29 +43,21 @@ VOID CommutativityChecker::parseTasksIR(char * IRlogName) {
       continue;
     } // if
 
-    if ( isCriticalSectionStart(sttmt) ) { // else a new task name
-#ifdef DEBUG
-      std::cout << "Task name: " << sttmt << std::endl;
-#endif
+    if ( isCriticalSectionStart(sttmt) ) {      // new critical sec.
       currentTask = std::vector<Instruction>();
     }
 
-    if (isCriticalSectionEnd(sttmt) ) {
+    if ( isCriticalSectionEnd(sttmt) ) {        // end critical sec.
       Tasks.insert( currentTask );
     }
-#ifdef DEBUG
-    // got here :-(, wierd std::string!
-    std::cout << "Unexpected program statement: " << sttmt << std::endl;
-#endif
   } // end while
   IRcode.close();
-  std::cout << "No. critical sections in IIR: " << Tasks.getSize()
-            << std::endl;
+  std::cout << "No. of critical sections in IIR: " 
+            << Tasks.getSize() << std::endl;
 }
 
-
 /**
- * Checks for commutative task operations which have been
+ * Checks for commutative critical sections operations which have been
  * flagged as conflicts.
  */
 bool CommutativityChecker::isCommutative(const Conflict & conflict) {
@@ -71,26 +66,17 @@ bool CommutativityChecker::isCommutative(const Conflict & conflict) {
   if (conflict.action1.isWrite != conflict.action2.isWrite) {
     return false;
   }
-#ifdef DEBUG
-  std::cout << task1 << " <--> "<< task2 << std::endl;
-#endif
   INTEGER line1 = conflict.action1.lineNo;
   INTEGER line2 = conflict.action2.lineNo;
-#ifdef DEBUG
-  std::cout << "Lines " << line1 << " <--> " << line2 << std::endl;
-#endif
   operationSet.clear(); // clear set of commuting operations
 
   // check if line1 operations commute & line2 operations commute
   if ( involveSimpleOperations( /*function ID*/ line1 ) &&
        involveSimpleOperations( /*function ID*/ line2 ) ) {
     return true;
-#ifdef DEBUG
-    std::cout << "THERE IS SAFETY line1: " << line1 << " line2: "
-              << line2 << std::endl;
-#endif
+  } else {
+    return false;
   }
-  return false;
 }
 
 BOOL CommutativityChecker::involveSimpleOperations(
@@ -109,10 +95,7 @@ BOOL CommutativityChecker::involveSimpleOperations(
      if (i->lineNo == lineNumber) instr  = *i;
      index++;
   }
-#ifdef DEBUG
-  std::cout << "SAFET " << taskName << " " << instr.destination
-            << ", idx: "<< index << std::endl;
-#endif
+  
   // expected to be a store
   if (instr.oper == STORE) {
     return isSafe(taskBody->getCriticalSectionBody(),
@@ -128,8 +111,12 @@ bool CommutativityChecker::isSafe(
     INTEGER loc,
     std::string operand) {
 
-  if (loc < 0) return true;
-
+  if (loc < 0) {
+    if ( !operationSet.size() )              return false;
+    else if ( operationSet.isCommutative() ) return true;
+    else                                     return false;
+  }
+  
   Instruction instr = taskBody.at(loc);
   if (instr.oper == ALLOCA && instr.destination == operand) {
       return true;
