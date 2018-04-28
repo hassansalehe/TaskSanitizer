2a25d304a699ae4ea0d97311a317bdb890850566
hmatar <hassansalehe@gmail.com>
2018-04-28 23:10:40 +0300

    Add fibonacci with memoization

--- src/tests/benchmarks/Fibonacci.cc
+++ src/tests/benchmarks/Fibonacci.cc
@@ -9,42 +9,40 @@
 #include <stdio.h>
 #include <omp.h>
 
+int * memoizer;
 long comp_fib_numbers(int n) {
-  if ( n == 0 || n == 1 ) return(n);
+  if ( n == 0 || n == 1 ) return n;
 
-  // In case the sequence gets too short, execute the serial version
-  if ( n < 20 ) {
-    return(comp_fib_numbers(n-1)+comp_fib_numbers(n-2));
-  } else {
-    // Basic algorithm: f(n) = f(n-1) + f(n-2)
-    long fnm1, fnm2, fn;
+  #pragma omp task firstprivate(n)
+  {
+    if (memoizer[n - 1] == 0) memoizer[n - 1] = comp_fib_numbers(n - 1);
+  }
 
-#pragma omp task shared(fnm1)
-      fnm1 = comp_fib_numbers(n-1);
+  #pragma omp task firstprivate(n)
+  {
+    if (memoizer[n - 2] == 0) memoizer[n - 2] = comp_fib_numbers(n - 2);
+  }
 
-#pragma omp task shared(fnm2)
-      fnm2 = comp_fib_numbers(n-2);
+  #pragma omp taskwait
+  int fn = memoizer[n - 1] + memoizer[n - 2];
 
-#pragma omp taskwait
-      fn = fnm1 + fnm2;
-
-    return (fn);
- }
+  return fn;
 }
 
 int main(int argc, char **argv) {
   int n;
   long result;
-  if (argc<2) {
+  if (argc < 2) {
     printf("usage ./fibonacci <number>\n");
     exit(1);
   }
   n = atoi(argv[1]);
 
+  memoizer = new int[n+1]();
 //#pragma omp parallel num_threads(NUM_THREADS)
-#pragma omp parallel
+#pragma omp parallel num_threads(2)
   {
-#pragma omp single nowait
+#pragma omp single
     {
       result = comp_fib_numbers(n);
     } // end of single region
