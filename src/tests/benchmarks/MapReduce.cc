79215c99f18db285a2bd6906b0480a9a810b3a0c
hmatar <hassansalehe@gmail.com>
2018-04-29 15:32:24 +0300

    rewrite MapReduce for meaningful race report

--- src/tests/benchmarks/MapReduce.cc
+++ src/tests/benchmarks/MapReduce.cc
@@ -36,64 +36,153 @@
 #include <iterator>
 #include <algorithm>
 
-#define NTHREADS 4
+#include <string.h>
+#include <assert.h>
 
-void prepareInputs(std::vector<std::string> &words, char *file) {
+#define NTHREADS 2
+#define WORDS 2048
+#define WORDSIZE 50
+
+typedef struct vector {
+  int capacity = 0;
+  int size = 0;
+  char words[WORDS][WORDSIZE];
+
+  vector() {
+    capacity = WORDS;
+    size = 0;
+    //words = (char**) malloc(capacity * sizeof(char *));
+  }
+
+  int push_back(const char * word) {
+    if (size == capacity || strlen(word) >= WORDSIZE) return -1;
+
+    int pos = size++;
+    strcpy(words[pos], word);
+    return pos;
+  }
+
+  // returns position of the pushed word
+  int push_unique(const char * word) {
+
+    if (size == capacity || strlen(word) >= WORDSIZE) return -1;
+    if (word == NULL) return -1;
+
+    for (int pos = 0; pos < size; ++pos) {
+      if ( words[pos] && 0 == strcmp(words[pos], word) ) {
+        return pos;
+      }
+    }
+
+    int pos = size++;
+    strcpy(words[pos], word);
+    return pos;
+  }
+
+  bool isFull()           const { return size == capacity; }
+  int getSize()           const { return size; }
+  int getCapacity()       const { return capacity; }
+  const char *at(int pos) const { return words[pos]; }
+
+  void print() {
+    for (int i = 0; i < size; i++) {
+      printf("%s\n", words[i]);
+    }
+  }
+
+} vector;
+
+
+typedef struct map {
+  vector words;
+  int counts[WORDS];
+  int capacity;
+
+  map() {
+    memset(counts, 0, sizeof(int) * words.getCapacity());
+    capacity = words.getCapacity();
+  }
+
+  const char *atWord(int pos) const {
+    assert(pos < words.getSize());
+    return words.at(pos);
+  }
+
+  int atCount(int pos) const {
+    assert(pos < words.getSize());
+    return counts[pos];
+  }
+
+  int getSize() const { return words.getSize(); }
+
+  void addCount(const char *word, int precount) {
+    int pos = words.push_unique(word);
+    if(pos != -1) {
+      counts[pos] += precount;
+    }
+  }
+
+  void print() {
+    for (int i = 0; i < words.getSize(); ++i) {
+      printf("%s: %d\n", words.at(i), counts[i]);
+    }
+  }
+
+} map;
+void prepareInputs(vector &words, char *file) {
 
   std::ifstream text( file );
   std::string line;
 
   while ( getline( text, line ) ) {
     std::istringstream splits( line );
-    copy( std::istream_iterator< std::string >( splits ),
-          std::istream_iterator< std::string >(),
-          std::back_inserter( words )
-        );
+    std::string word;
+    while (splits >> word) {
+      words.push_back( word.c_str() );
+    }
   }
+
+  //words.print();
 }
 
-void splitInput(std::vector<std::string> &words,
-                std::vector<std::string> &sub_words,
-                int pos) {
-  int chunk_size = words.size() / NTHREADS;
+void splitInput(vector &words, vector &sub_words, int pos) {
+  int chunk_size = words.getSize() / NTHREADS;
   int start      = chunk_size * pos;
   int end        = start + chunk_size;
 
   while (start < end) {
-    sub_words.push_back( words[ start++ ] );
+    sub_words.push_back( words.at(start++) );
   }
 }
 
-void mapWords(std::vector<std::string> &words,
-              std::map<std::string, int> &hist) {
-  for (auto word = words.begin(); word != words.end(); word++) {
-    hist[ *word ] += 1;
+void mapWords(vector &words, map &hist) {
+  for (int pos = 0; pos < words.getSize(); ++pos) {
+    hist.addCount( words.at(pos), 1 );
+  }
+  //hist.print();
+}
+
+void updateHistogram(map &histogram, const map &word_hist) {
+
+  for (int pos = 0; pos != word_hist.getSize(); pos++) {
+    const char *word   =  word_hist.atWord(pos);
+    int     precount   =  word_hist.atCount(pos);
+    histogram.addCount(word, precount);
   }
 }
 
-void updateHistogram(std::map<std::string, int> &histogram,
-                     std::map<std::string, int> &word_hist) {
-
-  for (auto elem = word_hist.begin(); elem != word_hist.end(); elem++) {
-    std::string word   =  elem->first;
-    int     precount   =  elem->second;
-    histogram[ word ] +=  precount;
-  }
-}
-
-void printHistogram(const std::map<std::string, int> & histogram) {
-  for (auto word = histogram.begin();
-       word != histogram.end();
-       word++) {
-    std::cout << word->first << ": " << word->second << std::endl;
+void printHistogram(const map &histogram) {
+  for ( int pos = 0; pos < histogram.getSize(); ++pos) {
+    std::cout << histogram.atWord(pos)  << ": "
+              << histogram.atCount(pos) << std::endl;
   }
 }
 
 int main(int argc, char* argv[]) {
-  std::map<std::string, int>  histogram;
-  std::map<std::string, int>  sub_hist[NTHREADS];
-  std::vector<std::string>    sub_words[NTHREADS];
-  std::vector<std::string>    words;
+  map    histogram;
+  map    sub_hist[NTHREADS];
+  vector sub_words[NTHREADS];
+  vector words;
 
   if (argc < 2 ) {
     std::cout << "ERROR!! No input file given" << std::endl;
@@ -102,7 +191,7 @@ int main(int argc, char* argv[]) {
   }
   prepareInputs(words, argv[1]);
 
-  #pragma omp parallel num_threads(NTHREADS)  \
+  #pragma omp parallel num_threads(NTHREADS) shared(words)  \
   shared(sub_hist) shared(sub_words) shared(histogram)
   {
     #pragma omp single
@@ -110,9 +199,10 @@ int main(int argc, char* argv[]) {
       // split words into 4 buckets
       for (int i = 0; i < NTHREADS; i++) {
         #pragma omp task depend(out: sub_words[i])  \
-        firstprivate(i)  shared(sub_words)
+        firstprivate(i)
         {
-          splitInput(words, sub_words[i], i);
+           splitInput(words, sub_words[i], i);
+           std::cout << "split thread: " << omp_get_thread_num() << "\n";
         }
       }
 
@@ -126,8 +216,7 @@ int main(int argc, char* argv[]) {
       }
 
       for (int i = 0; i < NTHREADS; i++) {
-        #pragma omp task depend(in: sub_hist[i]) depend(out: histogram)   \
-        shared(sub_hist) shared(histogram)
+        #pragma omp task depend(in: sub_hist[i])
         { // Reduce task
           updateHistogram(histogram, sub_hist[i]);
           std::cout << "reduce thread: " << omp_get_thread_num() << "\n";
@@ -135,15 +224,14 @@ int main(int argc, char* argv[]) {
         }
       }
 
-      #pragma omp task depend(in: histogram) shared(histogram)
+      #pragma omp taskwait
+      #pragma omp task //depend(in: histogram) shared(histogram)
       { // Printer task
         std::cout << "printer thread no: " << omp_get_thread_num()
-                  << "\n Total words: "    << histogram.size()
+                  << "\n Total words: "    << histogram.getSize()
                   << std::endl;
                   printHistogram(histogram);
       }
-
-      #pragma omp taskwait
     }
   } // end parallel omp
 
