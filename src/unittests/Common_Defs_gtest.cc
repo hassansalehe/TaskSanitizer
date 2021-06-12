ed8a082c7357cc5c2f9991dfe7fc18f7aa1becbe
Hassan @Ubuntu <hassansalehe@gmail.com>
2021-06-13 00:07:11 +0200

    Add test and fix build issues

---
+++ src/unittests/Common_Defs_gtest.cc
@@ -0,0 +1,20 @@
+#include "common/defs.h"
+#include <gtest/gtest.h>
+
+//! \brief Test that OperRepresentation converts operations
+//!        to approriate string representations.
+TEST(OperRepresentation, TestConversion) {
+  ASSERT_EQ("ADD", OperRepresentation(OPERATION::ADD));
+  ASSERT_EQ("SUB", OperRepresentation(OPERATION::SUB));
+  ASSERT_EQ("MUL", OperRepresentation(OPERATION::MUL));
+  ASSERT_EQ("DIV", OperRepresentation(OPERATION::DIV));
+  ASSERT_EQ("RET", OperRepresentation(OPERATION::RET));
+  ASSERT_EQ("SHL", OperRepresentation(OPERATION::SHL));
+  ASSERT_EQ("CALL", OperRepresentation(OPERATION::CALL));
+  ASSERT_EQ("LOAD", OperRepresentation(OPERATION::LOAD));
+  ASSERT_EQ("STORE", OperRepresentation(OPERATION::STORE));
+  ASSERT_EQ("ALLOCA", OperRepresentation(OPERATION::ALLOCA));
+  ASSERT_EQ("BITCAST", OperRepresentation(OPERATION::BITCAST));
+  ASSERT_EQ("GETELEMENTPTR", OperRepresentation(OPERATION::GETELEMENTPTR));
+}
+
